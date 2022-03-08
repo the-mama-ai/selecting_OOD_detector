dbb7d7b1e1e5d51ba5958b7fa0e57ec4a534203c
Karina Zadorozhny <karinazad@Karinas-MacBook-Pro.local>
2022-03-08 17:18:41 -0600

    Add init files, remove DUE

--- selecting_OOD_detector/__init__.py
+++ selecting_OOD_detector/__init__.py
@@ -1,2 +1,12 @@
-# from .pipeline.ood_pipeline import OODPipeline
-# from .pipeline.tuner import HyperparameterTuner
+try:
+    from .pipeline.ood_pipeline import OODPipeline
+    from .pipeline.tuner import HyperparameterTuner
+
+except:
+    try:
+        from pipeline.ood_pipeline import OODPipeline
+        from pipeline.tuner import HyperparameterTuner
+
+    except:
+        from selecting_OOD_detector.pipeline.ood_pipeline import OODPipeline
+        from selecting_OOD_detector.pipeline.tuner import HyperparameterTuner
