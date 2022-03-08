bef1ae2aa3b462ecc8fbd95f90864b42d73a3d66
Karina Zadorozhny <karinazad@Karinas-MacBook-Pro.local>
2022-03-08 17:23:08 -0600

    Add init

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="selecting_ood_detector",
     long_description_content_type="text/markdown",
-    version="0.1",
+    version="0.1.1",
     py_modules=['selecting_OOD_detector'],
     packages=find_packages(),
     url="https://github.com/the-mama-ai/selecting_OOD_detector",
